a88369463197b9eb1edffcf90d268f361fd41b71
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-09-06 10:05:41 +0300

    create a test user class for the various test cases and a setUp function to be run before all the other test cases

--- user_test.py
+++ user_test.py
@@ -1,2 +1,13 @@
 import unittest
 from user import User
+
+
+class TestUser(unittest.TestCase):
+    """class that defines test cases for the user class"""
+
+    def setUp(self):
+        """
+        defines instructions that would be executed before each test cases
+        """
+        self.new_user = User(
+            'Derrick', 'Odhiambo', 'odhiamboderrick56@gmail.com', 'namteroh', 'test123')
