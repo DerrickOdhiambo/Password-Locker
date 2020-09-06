1d72b545f9adfae1289250b2838ac4508dda9864
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-09-06 09:48:42 +0300

    create a user class with 5 parameters for creating new user object

---
+++ user.py
@@ -0,0 +1,11 @@
+class User:
+    """
+    User class for creating password locker account and logging in
+    """
+
+    def __init__(self, first_name, last_name, email, username, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.username = username
+        self.password = password
