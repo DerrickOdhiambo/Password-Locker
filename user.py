b218f678a06e668d12bf391b0001b436efd182b4
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-09-06 18:53:46 +0300

    create a funtion to test if user credentials are being appended to the lis

--- user.py
+++ user.py
@@ -6,11 +6,8 @@ class User:
     def __init__(self, first_name, last_name, email, username, password):
         self.first_name = first_name
         self.last_name = last_name
-        self.email = email
         self.username = username
         self.password = password
-
-    user_details = []  # empty user details list
 
     def save_user(self):
         """
--- user_test.py
+++ user_test.py
@@ -10,7 +10,7 @@ class TestUser(unittest.TestCase):
         defines instructions that would be executed before each test cases
         """
         self.new_user = User(
-            'Derrick', 'Odhiambo', 'odhiamboderrick56@gmail.com', 'namteroh', 'test123')
+            'Derrick Odhiambo', 'namteroh', 'test123')
 
     # 1st test
     def test_init(self):
@@ -18,11 +18,15 @@ class TestUser(unittest.TestCase):
         Test if the object has been initialized corretly
         """
 
-        self.assertEqual(self.new_user.first_name, 'Derrick')
-        self.assertEqual(self.new_user.last_name, 'Odhiambo')
-        self.assertEqual(self.new_user.email, 'odhiamboderrick56@gmail.com')
+        self.assertEqual(self.new_user.fullname, 'Derrick Odhiambo')
         self.assertEqual(self.new_user.username, 'namteroh')
         self.assertEqual(self.new_user.password, 'test123')
+
+    # 2nd test if the user details are saved
+    def test_save_user(self):
+
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_credentials), 1)
 
 
 if __name__ == '__main__':
