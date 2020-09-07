81926f02a242446b51602eefb37e45d7a1270f42
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-09-07 21:38:33 +0300

    add a semicolon to a command statement

--- run.py
+++ run.py
@@ -147,7 +147,7 @@ Use the following commands to procceed:
 
                 while True:
                     print(
-                        'Proceed to manage your account. Use the following short-codes to navigate through the application')
+                        'Proceed to manage your account. Use the following short-codes to navigate through the application :')
                     print("""
           nw - create new account credentials
           vw - view existing credentials
